fc4621199df6aeb3cf9cccdbda2fc753660ab198
Alexander Hanke <alexander.hanke@ntnu.no>
2026-07-16 09:27:01 +0200

    Added virtual destructor to interpolation

--- src/interpolation.h
+++ src/interpolation.h
@@ -29,7 +29,7 @@ class dive;
 class interpolation
 {
 public:
-
+    virtual ~interpolation() = default;
     virtual void start(lexer*,dive*,int,double*,double*,double*,double*,double*,int,int,double**)=0;
 };
 
