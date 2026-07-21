1929fe16aa0fc55303915a5068374333ba680a0d
Alexander Hanke <alexander.hanke@ntnu.no>
2026-07-21 12:06:13 +0200

    Formatted print_stl_geodat

--- src/print_stl_geodat.cpp
+++ src/print_stl_geodat.cpp
@@ -36,24 +36,22 @@ print_stl_geodat::~print_stl_geodat()
 void print_stl_geodat::print_geo(lexer* p, dive* a)
 {
     mkdir("./DIVEMesh_Geo",0777);
-	sprintf(name,"./DIVEMesh_Geo/REEF3D_stl_geo.dat");
+    char name[100];
+    snprintf(name,sizeof(name),"./DIVEMesh_Geo/REEF3D_stl_geo.dat");
 
     ofstream result;
     result.open(name, ios::binary);
 
     if(p->S91==1)
-	XYLOOP
-	result<<p->XP[IP]<<" "<<p->YP[JP]<<" "<<a->zstl_min(i,j)<<endl;
+    {
+        XYLOOP
+        result<<p->XP[IP]<<" "<<p->YP[JP]<<" "<<a->zstl_min(i,j)<<endl;
+    }
+    else if(p->S91==2)
+    {
+        XYLOOP
+        result<<p->XP[IP]<<" "<<p->YP[JP]<<" "<<a->zstl_max(i,j)<<endl;
+    }
 
-    if(p->S91==2)
-	XYLOOP
-	result<<p->XP[IP]<<" "<<p->YP[JP]<<" "<<a->zstl_max(i,j)<<endl;
-
-
-
-	result.close();
+    result.close();
 }
-
-
-
-
